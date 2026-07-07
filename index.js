94a7985ca4d256140723d19eed98a45f545b1080
Fernando <157437731+NotXina@users.noreply.github.com>
2026-07-06 23:25:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // @name         MultBot
 // @author       NotXina
 // @description  ModernBot aprimorado com módulos adicionais para Grepolis
-// @version      1.3.0
+// @version      1.3.1
 // @match        http://*.grepolis.com/game/*
 // @match        https://*.grepolis.com/game/*
 // @grant        none
